ebe00dc1f8a366565d85a3dee964810fc4d41c59
Pavan Hiware <pavanhiware11@gmail.com>
2025-09-24 21:51:45 +0530

    Add files via upload

---
+++ projectDetails/src/test/java/cucumberOption/TestNG_testRunner.java
@@ -0,0 +1,20 @@
+package CucumberOption;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="src/test/java/features",glue="StepDefinationsCucumberPracticeProject",monochrome=true,
+plugin= {"html:target/cucumber.html","json:target/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+tags=("@SearchTest"))
+
+public class TestNG_testRunner extends AbstractTestNGCucumberTests{
+	
+//	@Override
+//	@DataProvider(parallel=true)
+//	public Object[][] scenarios(){
+//		return super.scenarios();
+//	}
+}
